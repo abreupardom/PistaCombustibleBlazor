40cc7204da7141be884a2d0845cad281eceea4d3
michel <abreupardom@allnovu.net>
2026-04-01 16:52:59 -0400

    Arreglo de elimiar  con el popup

--- obj/Debug/net10.0/PistaCombustible.AssemblyInfo.cs
+++ obj/Debug/net10.0/PistaCombustible.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PistaCombustible")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfc511ae636746f0d0968b59d1b2a5ba9e17ab41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aad668139fd4ab4a8a566459941dd70c4d9c9370")]
 [assembly: System.Reflection.AssemblyProductAttribute("PistaCombustible")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PistaCombustible")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
