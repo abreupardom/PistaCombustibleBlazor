31fb54d32704a9fe293ba634d7415eb4bd9885e3
michel <abreupardom@allnovu.net>
2026-04-01 11:28:50 -0400

    mejoras en notificacion global y que sea componente reutilizable, mejoras en ionterfaz  y dashboard

--- obj/Debug/net10.0/PistaCombustible.AssemblyInfo.cs
+++ obj/Debug/net10.0/PistaCombustible.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PistaCombustible")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+625f849fa7c26d0a8600503d73d0d76ccf1e8310")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1accf325f8a83d9639d84113272c7ada48da84c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PistaCombustible")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PistaCombustible")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
