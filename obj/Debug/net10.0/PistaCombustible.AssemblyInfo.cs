8157f12e72e46285cf6083ac1e13d45e4284d3dc
michel <abreupardom@allnovu.net>
2026-04-02 09:50:12 -0400

    cambiar cartel cuando la busqueda en la tabla no exista lo que se busca

--- obj/Debug/net10.0/PistaCombustible.AssemblyInfo.cs
+++ obj/Debug/net10.0/PistaCombustible.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PistaCombustible")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40cc7204da7141be884a2d0845cad281eceea4d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+241d28b6b2485b88e0ace33966d44fbe44cd5ece")]
 [assembly: System.Reflection.AssemblyProductAttribute("PistaCombustible")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PistaCombustible")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
