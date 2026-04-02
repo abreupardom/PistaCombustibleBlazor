241d28b6b2485b88e0ace33966d44fbe44cd5ece
michel <abreupardom@allnovu.net>
2026-04-02 09:15:51 -0400

    arreglo de  cartel es espannol d elos ocmbobox cuando no hay resultados en la busqueda

--- obj/Debug/net10.0/PistaCombustible.AssemblyInfo.cs
+++ obj/Debug/net10.0/PistaCombustible.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PistaCombustible")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aad668139fd4ab4a8a566459941dd70c4d9c9370")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40cc7204da7141be884a2d0845cad281eceea4d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PistaCombustible")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PistaCombustible")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
